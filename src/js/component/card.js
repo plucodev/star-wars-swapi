ebd964636e7d4f53d90bcfff34a8c688aed53d61
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-06-26 22:30:50 +0000

    fixed issues

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,5 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export const Card = () => (
-	<h1>Card</h1>
-);
+export const Card = props => {
+	return (
+		<div className="card custom-card">
+			<img src="..." className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">
+					Some quick example text to build on the card title and make up the bulk of the card content.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	title: PropTypes.string
+};
